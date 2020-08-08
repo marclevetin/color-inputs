6e1d76dd13e582dedc721d421f0b07962f506c95
marclevetin <marc@derbyhat.org>
2020-08-08 12:53:06 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+// console.log(myOtherFunction)
+import React from "react";
+
+// export default () => (
+//   <>
+//     <h1>Welcome to React Parcel Micro App!</h1>
+//     <p>Hard to get more minimal than this React app.</p>
+//   </>
+// );
+export default function App() { 
+  return (
+    <>
+      <h1>Welcome to React Parcel Micro App!</h1>
+      <p>Hard to get more minimal than this React app.</p>
+    </>
+  )
+};
+// const App = () => (
+//     <>
+//       <h1>Welcome to React Parcel Micro App!</h1>
+//       <p>Hard to get more minimal than this React app.</p>
+//     </>
+// );
+
+// export default App;
+
+
+// //arrow
+// // 'this' is different
+// const myfunction = () => 'allTheThings'
+// const myfunction123 = () => ('allTheThings')
+
+// () => ()
+
+// const myfunction = () => {
+//   return 'allTheThings'
+// }
+// myfunction2 = () => {'allTheThings'}
+
+// // the function way?
+// function myOtherFunction() {
+//   return 'allTheThings'
+// }
+
+// // ES6
+// // scoping
+// let thing2 = 3 // reassign
+// const thing3 = 3// no reassignment
+
+
+// var thing = 2
